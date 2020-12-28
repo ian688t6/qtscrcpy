bdd3d5484d70fd51263ef6d4c307deaad15d8811
ian <ian688t6@foxmail.com>
2020-12-28 15:09:23 +0800

    add Decoder::deinit when application quit

--- main.cpp
+++ main.cpp
@@ -9,5 +9,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Dialog w;
     w.show();
-    return a.exec();
+    int ret = a.exec();
+    Decoder::deinit();
+    return ret;
 }
